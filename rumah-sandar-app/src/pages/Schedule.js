60c0c671f15e3ec293c668ed5f9ba7f4709afbe4
mfachrulph21 <mfachrulph21@gmail.com>
2022-11-04 03:51:43 +0700

    20% proggress

--- rumah-sandar-app/src/pages/Schedule.js
+++ rumah-sandar-app/src/pages/Schedule.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import Calendar from 'react-calendar';
 
 export default function Schedule() {
+    const [value, onChange] = useState(new Date());
+    console.log(value, '<<<<<< INI VALUE')
     return (
+        <>
+        <Container>
+
+        <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+        </Container>
         
+        </>
     )
 }
--- rumah-sandar-app/src/route/index.js
+++ rumah-sandar-app/src/route/index.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom'  
 import LandingPage from '../pages/LandingPages'
 import OrphansList from '../pages/OrphansList'
+import Schedule from '../pages/Schedule'
 
 const router =  createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router =  createBrowserRouter([
     {
         path: '/orphansList',
         element: <OrphansList/>
+    },
+    {
+        path:'/schedule',
+        element: <Schedule/>
     }
 ])
 
