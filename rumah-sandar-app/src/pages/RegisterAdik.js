91759934e7ac74be3edbfa2d541ba6c4efab9791
lannyalfiani <lannyalfiani13@gmail.com>
2022-11-08 00:46:11 +0700

    on change register form orphan

--- rumah-sandar-app/src/pages/RegisterAdik.js
+++ rumah-sandar-app/src/pages/RegisterAdik.js
@@ -1,38 +1,85 @@
+import { useState } from 'react';
 import { Container, Form, Button, Card, Modal, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { ArrowBarRight, ArrowBarLeft } from 'react-bootstrap-icons';
 
 const RegisterAdik = () => {
   const navigate = useNavigate();
+
+  const [registerForm, setRegisterForm] = useState({
+    email: "",
+    password: "",
+    fullName: "",
+    imageUrl: "",
+    OrphanageId: 0
+  })
+
+  function handleChange(e) {
+    e.preventDefault()
+
+    let data = e.target.name
+    let value = e.target.value
+
+    let newRegisterForm = {
+      ...registerForm
+    }
+
+    newRegisterForm[data] = value
+    setRegisterForm(newRegisterForm)
+  }
+
+  console.log(registerForm);
+
   return (
     <Container className="mt-5">
       <Card>
         <Card.Body>
-          <h3 className="text-center">Register as Orphans</h3>
+          <h3 className="text-center">Daftar sebagai adik asuh</h3>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                value={registerForm.email}
+                onChange={handleChange}
+                name="email"
+                type="email"
+                placeholder="Enter email" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Fullname</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
+              <Form.Control
+                value={registerForm.fullName}
+                onChange={handleChange}
+                name="fullName"
+                type="text" placeholder="Enter username" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control
+                value={registerForm.password}
+                onChange={handleChange}
+                name="password"
+                type="password" placeholder="Enter password" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3">
               <Form.Label>Image</Form.Label>
-              <Form.Control type="file" />
+              <Form.Control
+                value={registerForm.imageUrl}
+                onChange={handleChange}
+                name="imageUrl"
+                type="file" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Oprhanages</Form.Label>
-              <Form.Select aria-label="Default select example">
+              <Form.Select
+                onChange={handleChange}
+                name="OrphanageId"
+                value={registerForm.OrphanageId}
+                aria-label="Default select example">
                 <option selected disabled>
                   Select
                 </option>
