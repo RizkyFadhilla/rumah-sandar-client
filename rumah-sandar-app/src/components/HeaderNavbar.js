b11a6a6fc9e65553ac189782bfe394d02376748b
RizkyFadhilla <rizkyfadhilla123@gmail.com>
2022-11-08 22:41:20 +0700

    merging awal

--- rumah-sandar-app/src/components/HeaderNavbar.js
+++ rumah-sandar-app/src/components/HeaderNavbar.js
@@ -1,9 +1,5 @@
-import { useEffect } from 'react';
-import { Container, Nav, Navbar, Image, NavDropdown } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import LogoRumahSandar from '../assets/logo-rumah-sandar.png';
-import { fetchClassCategories, requestMatchOrphan } from '../redux/user';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchClassCategories, requestMatchOrphan } from "../redux/user";
 import { useEffect } from "react";
 import {
   Container,
@@ -48,28 +44,25 @@ const HeaderNavbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const{ loginUser } = useSelector((state) => {
-    return state.user
-  })
-
+  const { loginUser } = useSelector((state) => {
+    return state.user;
+  });
 
   function logoutHandler(e) {
     e.preventDefault();
 
-    localStorage.clear()
-    dispatch(fetchClassCategories())
-    navigate('/')
-    
+    localStorage.clear();
+    dispatch(fetchClassCategories());
+    navigate("/");
   }
 
   function requestMatch(e) {
     e.preventDefault();
-    
+
     dispatch(requestMatchOrphan()).then(() => {
       //swal disini kalo request matchnya udah disampaikan
-      console.log('match requested')
-
-    })
+      console.log("match requested");
+    });
   }
 
   // const access_token = loginUser?.access_token
@@ -77,14 +70,15 @@ const HeaderNavbar = () => {
   // const isMacthed = loginUser.sendData?.matchStatus
   // const username = loginUser.sendData?.fullName
   // const imageProfile = loginUser.sendData?.imageUrl
-  const access_token = localStorage.access_token
-  const role = localStorage.role
-  const isMatched = localStorage.isMatched
-  const username = localStorage.username
-  const imageProfile = localStorage.image
-  
+  const access_token = localStorage.access_token;
+  const role = localStorage.role;
+  const isMatched = localStorage.isMatched;
+  const username = localStorage.username;
+  const imageProfile = localStorage.image;
+  const shareUrl = "https://rumah-sandar.web.app/";
+  const title = "Rumah Sandar";
   // if(!access_token){
-  //   <Navbar bg="light" variant="light" className="navbar" sticky="top"></Navbar>  
+  //   <Navbar bg="light" variant="light" className="navbar" sticky="top"></Navbar>
   // }
   return (
     <Navbar bg="light" variant="light" className="navbar" sticky="top">
@@ -154,18 +148,39 @@ const HeaderNavbar = () => {
                 </NavDropdown.Item>
               </NavDropdown>
             )}
-
-          {access_token && role === 'volunteer' && isMatched === 'notMatch' && <Nav.Link onClick={() => navigate('/orphansList')}>Daftar Adik</Nav.Link>}
-          {access_token && isMatched === 'alreadyMatch' && <Nav.Link onClick={() => navigate('/schedule')}>Jadwal Kelas</Nav.Link>}
-          {access_token && isMatched === 'notMatch' && role === 'orphan' && <Nav.Link onClick={requestMatch}>Minta Pengajar</Nav.Link>}
-          {access_token && (
-            <Nav.Link className="ms-2" onClick={logoutHandler}>
-              Keluar
-            </Nav.Link>
-          )}
-          {access_token && <Nav.Link className="ms-2">Hello, {username}!</Nav.Link>}
-          {access_token && <Image src={imageProfile} width={'40'} height={'40'} roundedCircle={true} />}
-        </Nav>
+            {access_token &&
+              role === "volunteer" &&
+              isMatched === "notMatch" && (
+                <Nav.Link onClick={() => navigate("/orphansList")}>
+                  Daftar Adik
+                </Nav.Link>
+              )}
+            {access_token && isMatched === "alreadyMatch" && (
+              <Nav.Link onClick={() => navigate("/schedule")}>
+                Jadwal Kelas
+              </Nav.Link>
+            )}
+            {access_token && isMatched === "notMatch" && role === "orphan" && (
+              <Nav.Link onClick={requestMatch}>Minta Pengajar</Nav.Link>
+            )}
+            {access_token && (
+              <Nav.Link className="ms-2" onClick={logoutHandler}>
+                Keluar
+              </Nav.Link>
+            )}
+            {access_token && (
+              <Nav.Link className="ms-2">Hello, {username}!</Nav.Link>
+            )}
+            {access_token && (
+              <Image
+                src={imageProfile}
+                width={"40"}
+                height={"40"}
+                roundedCircle={true}
+              />
+            )}
+          </Nav>
+        </Row>
       </Container>
     </Navbar>
   );
